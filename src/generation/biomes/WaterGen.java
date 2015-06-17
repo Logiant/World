8f331d48c57d755dfc2f3a3b4e387c59e6f39322
Logan <giantpandagod@gmail.com>
2015-06-16 23:11:23 -0500

    Added WaterGen method outline

--- src/generation/biomes/WaterGen.java
+++ src/generation/biomes/WaterGen.java
@@ -13,6 +13,16 @@ public class WaterGen {
 
 	public float[][] generateRivers(float[][] heightmap) {
 		// TODO Auto-generated method stub
+		/*
+		 * Get the n highest points
+		 * chose m points (m <=n) to be river starts
+		 * flow rivers downhill
+		 * form mega-rivers
+		 * form/join existing lakes if a local minima is hit
+		 * flow into the sea
+		 * tributaries???
+		 * 
+		 */
 		return heightmap;
 	}
 }
