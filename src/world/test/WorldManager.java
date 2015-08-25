58c18f02aa79449549507a0825873f15ce054f1f
Logan <giantpandagod@gmail.com>
2015-08-25 17:15:03 -0500

    Minor world manager updates

--- src/world/test/WorldManager.java
+++ src/world/test/WorldManager.java
@@ -1,7 +1,10 @@
 package world.test;
 
+import java.io.IOException;
 import java.util.Random;
 
+import generation.GenTest2;
+import util.Vector3;
 import world.*;
 
 public class WorldManager {
@@ -15,7 +18,27 @@ public class WorldManager {
 									//unload regions which are shifted out (List<RegionManager> regionsToRemove) ?
 									//be prepared to write regions to disk if required... ?
 									//use threading to load unload - okay as long as regions are out of sight
-
+	GenTest2 mapgen;
+	float[][] map;
 	Random rGen;
+	
+	public void GenMap() {
+		try {
+			mapgen = new GenTest2();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		map = mapgen.getMap();
+	}
+	
+	public void Build(Vector3 playerPos) {
+		//calculate player x, z region location
+		int regionX = (int)(playerPos.x / RegionManager.NUM_CHUNKS / Chunk.CHUNK_WIDTH);
+		int regionZ = (int)(playerPos.z / RegionManager.NUM_CHUNKS / Chunk.CHUNK_DEPTH);
+		
+		//load chunks x+-n to z+-n
+		
+	}
 	
 }
